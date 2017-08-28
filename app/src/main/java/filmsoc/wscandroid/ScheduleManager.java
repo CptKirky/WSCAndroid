0d5c3d684cd824d8aeed49bae69e214806f1bf34
Richard Kirk <cptkirky101@gmail.com>
2017-08-28 21:57:29 +0100

    Even more comments! ScheduleManager is done, MainActivity is not finished yet

--- app/src/main/java/filmsoc/wscandroid/ScheduleManager.java
+++ app/src/main/java/filmsoc/wscandroid/ScheduleManager.java
@@ -5,14 +5,20 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 /**
- * This class manages the entire schedule, maintaining its ordering on addition of data.
+ * This class manages the entire schedule, maintaining its ordering on addition of data. A lot of
+ * functional stuff is done in this class.
  *
  * Created by Richard on 09/06/2016.
  */
 public class ScheduleManager {
 
-    private static Film[] schedule;
+    private static Film[] schedule;     //This is the array storing the schedule
 
+    /**
+     * This function returns the next film that is being shown.
+     * @return This returns the next film being shown (in O(n) time). Should the schedule be empty,
+     * null is returned. If there is only one film, that film is returned.
+     */
     private static Film getNextFilm(){
         Film result = null;
         for (int i = 0; i < schedule.length; i++) {
@@ -29,14 +35,26 @@ public class ScheduleManager {
         return result;
     }
 
+    /**
+     * This is used to determine if there is a film in the schedule.
+     * @return True is there is a film in the schedule, false otherwise.
+     */
     public static boolean isSchedule() {
         return (schedule != null);
     }
 
+    /**
+     * This returns all films in the stored state in the schedule.
+     * @return An array of films
+     */
     public static Film[] getSchedule() {
         return schedule;
     }
 
+    /**
+     * This is used to get the name of the next film that is being shown.
+     * @return The name of the next film. Should no film be in the schedule, then null is returned.
+     */
     public static String getNextFilmName() {
         Film result = null;
         if (schedule == null) {
@@ -47,6 +65,11 @@ public class ScheduleManager {
         return result.getName();
     }
 
+    //TODO: make the date return a Calendar object, rather than a long
+    /**
+     * This is used to get the date and time for the next film being shown.
+     * @return The date as a long.
+     */
     public static long getNextFilmDate() {
         Film result = null;
         if (schedule==null) {
@@ -62,6 +85,12 @@ public class ScheduleManager {
 
     }
 
+    /**
+     * This orders a given list of films by the first screening for the film. This uses a bubble
+     * sort...
+     * @param filmList The list fo films
+     * @return An array of films in first screening order
+     */
     public static Film[] orderSchedule(Film[] filmList) {
         boolean flag = true;
         while (flag) {
@@ -78,6 +107,12 @@ public class ScheduleManager {
         return filmList;
     }
 
+    /**
+     * This gets a list of all the films, and combines films and screenings together. It then
+     * orders the films. Each screening is a seperate element, rather than each film with multiple
+     * screenings.
+     * @return A list of films in order
+     */
     public static Film[] getNextFilms() {
         Film[] tmpList = getFilmsAfter(Calendar.getInstance());
         ArrayList<Film> result = new ArrayList<Film>();
@@ -109,6 +144,11 @@ public class ScheduleManager {
         return orderSchedule(result.toArray(new Film[result.size()]));
     }
 
+    /**
+     * This seperates the screenings into seperate film objects.
+     * @param tmpList The list of films where the screenigns need to be split
+     * @return The ordered array of screenings
+     */
     public static Film[] splitScreenings (Film[] tmpList) {
         if (tmpList == null) {
             return null;
@@ -143,6 +183,12 @@ public class ScheduleManager {
         return orderSchedule(result.toArray(new Film[result.size()]));
     }
 
+    /**
+     * This lists the films before a given date. All screenings in the film are the screenings
+     * before the date.
+     * @param date The date by which the screenings should be searched
+     * @return An array of films before the date
+     */
     public static Film[] getFilmsBefore(Calendar date) {
         ArrayList<Film> tmpList = new ArrayList<Film>();
         for (int i = 0; i < schedule.length; i++) {
@@ -158,6 +204,12 @@ public class ScheduleManager {
         return tmpList.toArray(new Film[tmpList.size()]);
     }
 
+    /**
+     * This lists the films after a given date. All screenings in the film are the screenings
+     * after the date.
+     * @param date The date by which the screenings should be searched
+     * @return An array of films after the date
+     */
     public static Film[] getFilmsAfter(Calendar date) {
         ArrayList<Film> tmpList = new ArrayList<Film>();
         for (int i = 0; i < schedule.length; i++) {
@@ -173,7 +225,13 @@ public class ScheduleManager {
         return tmpList.toArray(new Film[tmpList.size()]);
     }
 
-    //TODO: build getFilmsBetween function
+    /**
+     * This lists all the films shown between 2 dates. All screenings in the film between the 2
+     * dates are given as separate films.
+     * @param before The start date to be searched against
+     * @param after The end date to be searched against
+     * @return An array of screenings between the dates
+     */
     public static Film[] getFilmsBetween(Calendar before, Calendar after) {
         ArrayList<Film> tmpList = new ArrayList<Film>();
         for (int i = 0; i < schedule.length; i++) {
@@ -197,6 +255,13 @@ public class ScheduleManager {
         return tmpList.toArray(new Film[tmpList.size()]);
     }
 
+    /**
+     * This allows for a film to be added to the schedule. Should the film given be equal to null
+     * (i.e. not a valid film), then an exception is thrown. Should the film already exist, then,
+     * it will be merged with the current one.
+     * @param film The film to be added to the schedule
+     * @throws InvalidParameterException If the film given is null, this is thrown
+     */
     public static void addFilm(Film film) throws InvalidParameterException {
         if (film == null) {
             throw new InvalidParameterException("No film given");
@@ -221,6 +286,13 @@ public class ScheduleManager {
         }
     }
 
+    /**
+     * This allows for an array of films to be added to the schedule. Should the list not exist
+     * (i.e., it is null), then an exception is thrown. Should the film already exist, then,
+     * it will be merged with the current one.
+     * @param film The array of films to be added to the schedule
+     * @throws InvalidParameterException This is thrown should the input be null
+     */
     public static void addFilm(Film[] film) throws InvalidParameterException {
         if (film == null) {
             throw new InvalidParameterException("No films given");
@@ -262,6 +334,12 @@ public class ScheduleManager {
         }
     }
 
+    /**
+     * This gets a film details given a particular ID
+     * @param filmID The film ID
+     * @return The film object. Should there not be a schedule stored, or if the film is not
+     * present, null object is returned.
+     */
     public static Film getFilm(int filmID) {
         if (!isSchedule()) {
             return null;
@@ -274,6 +352,10 @@ public class ScheduleManager {
         return null;
     }
 
+    /**
+     * This gets the size of the currently-stored schedule.
+     * @return The current size of the schedule. Should no schedule be set-up, -1 is returned
+     */
     public static int getScheduleSize() {
         if (schedule == null) {
             return -1;
